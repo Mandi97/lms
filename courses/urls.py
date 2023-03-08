6312dde09df116ff24d811784c6c38f647c1223b
Mandi97 <pawel.mandrysz@gmail.com>
2023-03-08 15:12:21 +0100

    add course functions with permissions

--- courses/urls.py
+++ courses/urls.py
@@ -6,4 +6,8 @@ app_name = 'courses'
 
 urlpatterns = [
     path('', views.CourseListView.as_view(), name='courses-list'),
+    path('<int:pk>/', views.CourseDetailView.as_view(), name='course-detail'),
+    path('delete/<int:pk>/', views.CourseDeleteView.as_view(), name='course-delete'),
+    path('create/', views.CourseCreateView.as_view(), name='course-create'),
+
 ]
