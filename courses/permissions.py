de8e58c3e81ef880efd98d3448b99065dce2822b
Mandi97 <pawel.mandrysz@gmail.com>
2023-03-08 16:18:18 +0100

    edit course and create course for instructors

--- courses/permissions.py
+++ courses/permissions.py
@@ -5,6 +5,8 @@ class OwnerRequiredMixin(AccessMixin):
 
     def dispatch(self, request, *args, **kwargs):
         course = self.get_queryset().get(pk=kwargs.get('pk'))
-        if course.owner != request.user or not request.user.is_superuser:
+        if request.user.is_superuser:
+            return super().dispatch(request, *args, **kwargs)
+        if course.owner != request.user:
             return self.handle_no_permission()
         return super().dispatch(request, *args, **kwargs)
