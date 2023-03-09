5e848d589623c0c8435436864c1cafcfb668bb3e
Mandi97 <pawel.mandrysz@gmail.com>
2023-03-09 17:12:16 +0100

    few tests with coverage

---
+++ users/tests/test_models.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+@pytest.mark.slow
+def test_create_user(user, django_user_model):
+    users = django_user_model.objects.all()
+    assert len(users) == 2
+
+
+@pytest.mark.slow
+@pytest.mark.smoke
+def test_change_password(user, django_user_model):
+    user_db = django_user_model.objects.get(email='test@user.pl')
+
+    user_db.set_password('secret')
+    assert user_db.check_password('secret') is True
